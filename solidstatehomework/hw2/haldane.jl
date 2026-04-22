d31343e83d0566e878353ad7bf07f1fe32a8ccbc
Rexirium <rexirium@qq.com>
2026-04-22 23:57:20 +0800

    Haldane hamiltonian solved

--- solidstatehomework/hw2/haldane.jl
+++ solidstatehomework/hw2/haldane.jl
@@ -22,3 +22,19 @@ function updateHaldaneHamiltonian!(hmat::Matrix{ComplexF64}, kv::Vector{Float64}
     hmat[2, 1] = t1 * sum(cis.( Am * kv))
     hmat[1, 2] = conj(hmat[2, 1])
 end
+
+function eigenHaldane(kx::Matrix{Float64}, ky::Matrix{Float64}, t1::Real, t2::Number)
+    hmat = Matrix{ComplexF64}(undef, 2, 2)
+    lowerband = similar(kx)
+    upperband = similar(kx)
+    lowervecs = Array{ComplexF64}(undef, 2, size(kx)...)
+
+    for (i, kv) in enumerate(zip(kx, ky))
+        updateHaldaneHamiltonian!(hmat, kv, t1, t2)
+        eigs, eigvs = eigen(hmat)
+        lowerband[i] = real(eigs[1])
+        upperband[i] = real(eigs[2])
+        lowervecs[:, i] = eigvs[:, 1]
+    end
+    return lowerband, upperband, lowervecs
+end
